1682e7ab7dcbd2897a4e466116d74261a6c8f1da
Jennings Zhang <jenni_zh@protonmail.com>
2020-12-30 02:14:45 -0500

    Fix feed ID vs FS plugin instance ID confusion

--- src/app.js
+++ src/app.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const httpProxy = require('http-proxy');
 const dotenv = require('dotenv');
+const path = require('path');
 const colors = require('colors');
 const print = require('./print');
 const Cube = require('./cube');
 const UploadDetector = require('./store');
 
 dotenv.config();
+dotenv.config({path: path.resolve(process.cwd(), 'parameters/plugins.env')});
 
 const STORE_URL = process.env.REAL_STORE_URL;
 
@@ -27,18 +29,21 @@ app.all('/api/*', (req, res) => {
   proxy.web(req, res, {target: STORE_URL});
 });
 
-let feed = null;
+let fsInstanceId = null;
 
 /**
  * Get a singleton instance of the express app.
  * @return {Promise<Express>} express app
  */
 async function getApp() {
-  if (!feed) {
+  if (!fsInstanceId) {
     // find the FS plugin, then start the server
-    feed = await cube.searchFeed(process.env.FEED_NAME);
-    print(colors.dim(`found feed for "${feed.name}" - id=${feed.id}`));
-    new UploadDetector(cube, feed.id).attachTo(proxy);
+    const fsPlugin = await cube.searchPlugin(process.env.FS_PLUGIN_NAME);
+    const inst = await cube.get({ url: fsPlugin.instances });
+    fsInstanceId = inst.results[0].id;
+
+    print(colors.dim(`found ancestor plugin "${fsPlugin.name}" - id=${fsInstanceId}`));
+    new UploadDetector(cube, fsInstanceId).attachTo(proxy);
   }
   return app;
 }
